8e8398cbf539e6a6dfbd40c7f3e901c67bbb0eda
George van Heerden <george.vanheerden@sovtech.com>
2023-11-02 15:59:05 +0200

    feat(App): add swr logo & update styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { convertToTimeFormat, getButtonText } from './helpers'
 import { useInternetConnectivity } from './hooks/useInternetConnectivity'
 import { useTime } from './hooks/useTime'
 import reactLogo from '/assets/react.svg'
+import swrLogo from '/assets/swr.svg'
 import viteLogo from '/assets/vite.svg'
-import pizzaLogo from '/icons/ios/256.png'
 
 function App() {
   const { isOnline } = useInternetConnectivity()
@@ -22,33 +22,45 @@ function App() {
 
   return (
     <>
+      <h1>PWA with SWR cache demo</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <img src={pizzaLogo} className="logo pizza" alt="pizza" />
+        <a href="https://swr.vercel.app/" target="_blank">
+          <img src={swrLogo} className="logo swr" alt="SWR logo" />
+        </a>
+        {/* <img src={pizzaLogo} className="logo pizza" alt="pizza" /> */}
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>PWA with SWR cache demo</h1>
-      <h2>Browser time:</h2>
-      <h3>{browserTime}</h3>
-      <h2>API time:</h2>
-      <h3>
-        {timeIsUpdating ? 'Updating...' : convertToTimeFormat(time?.datetime)}{' '}
-      </h3>
+      <div className="container">
+        <div className="time-card">
+          {' '}
+          <h2>Browser time:</h2>
+          <h3>{browserTime}</h3>
+        </div>
+        <div className="time-card">
+          <h2>API time:</h2>
+          <h3>
+            {timeIsUpdating
+              ? 'Updating...'
+              : convertToTimeFormat(time?.datetime)}{' '}
+          </h3>
+        </div>
+      </div>
       {error ? (
         <div className="error">
           <code>{error}</code>
         </div>
       ) : null}
-      <div className="card">
+      <div className="container">
         <button onClick={updateTime}>
           {getButtonText(isOnline, timeIsUpdating)}
         </button>
       </div>
-      <code className="read-the-docs">
+      <code className="status">
         Status: {isOnline ? 'Online 🟢' : 'Offline 🔴'}
       </code>
     </>
